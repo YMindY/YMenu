76d0b6ba9cfe687fa7997217dc32c1a2a9df8613
YxMingY <526817087@qq.com>
2019-02-05 22:39:05 +0800

    #1 更新

--- src/yxmingy/ymenu/Main.java
+++ src/yxmingy/ymenu/Main.java
@@ -1,24 +1,33 @@
 package yxmingy.ymenu;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
+import yxmingy.yupi.UISender;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import cn.nukkit.Player;
+import cn.nukkit.block.BlockButtonStone;
 import cn.nukkit.command.*;
-import cn.nukkit.event.*;
 
 
-public class Main extends PluginBase implements Listener{
+public class Main extends PluginBase{
 	private Config conf;
 	public void onEnable() {
 		getLogger().notice("YMenu is Enabled! Author: xMing.");
 		conf = new Config(getDataFolder()+"/Config,yml",Config.YAML);
 		/*[
-		 * name=>
+		 * {菜单名}=>
 		 *   [
-		 *     {指令}=>(cmd),
-		 *     {图标}=>(file)
+		 *     {按钮名}=>
+		 *     [
+		 *       {指令}=>(cmd),
+		 *       {图标}=>(file)
+		 *     ]
 		 *   ]
 		 * ]
 		 */
-		getServer().getPluginManager().registerEvents(this, this);
+		getServer().getPluginManager().registerEvents(new Responser(), this);;
 	}
 	public void onDisable() {
 		getLogger().warning("YMenu is Turned Off!");
@@ -30,7 +39,16 @@ public class Main extends PluginBase implements Listener{
 			sender.sendMessage("没有名为"+args[0]+"的菜单！");
 			return true;
 		}
+		if(!(sender instanceof Player)) {
+			sender.sendMessage("控制台去死！");
+			return true;
+		}
+		LinkedHashMap<String, Object> data = new LinkedHashMap<String, Object>(),
+				                          buttons = new LinkedHashMap<String, Object>();
+		data.put("type", "form");
+		data.put("title", args[0]);
 		
+		UISender.sendUI(526817087, data, (Player)sender);
 		return true;
 	}
 }
---
+++ src/yxmingy/ymenu/Responser.java
@@ -0,0 +1,10 @@
+package yxmingy.ymenu;
+
+import cn.nukkit.Player;
+import yxmingy.yupi.ResponserBase;
+
+public class Responser extends ResponserBase{
+	public void response(int index,String data,Player player) {
+		
+	}
+}
