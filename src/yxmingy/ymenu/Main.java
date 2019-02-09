b04f9868a069a0aa0a4351a14510e3eefd0013a5
xMing <526817087@qq.com>
2019-02-09 20:51:17 +0800

    #3 测试配置

--- src/yxmingy/ymenu/Main.java
+++ src/yxmingy/ymenu/Main.java
@@ -14,7 +14,16 @@ public class Main extends PluginBase{
 	private Config conf;
 	public void onEnable() {
 		getLogger().notice("YMenu is Enabled! Author: xMing.");
-		conf = new Config(getDataFolder()+"/Config,yml",Config.YAML);
+		conf = new Config(getDataFolder()+"/Config.yml",Config.YAML);
+		LinkedHashMap<String, Object> data = new LinkedHashMap<String, Object>(),
+		                              menu = new LinkedHashMap<String, Object>(),
+		                              button = new LinkedHashMap<String, Object>();
+		button.put("指令","kill @s");
+		button.put("图标","无");
+		menu.put("自杀",button);
+		data.put("kill",menu);
+		conf.setAll(data);
+		conf.save();
 		/*[
 		 * {菜单名}=>
 		 *   [
@@ -32,8 +41,7 @@ public class Main extends PluginBase{
 		getLogger().warning("YMenu is Turned Off!");
 	}
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		int length = args.length;
-		if(length != 1) return false;
+		if(args.length != 1) return false;
 		if(!(conf.exists(args[0]))) {
 			sender.sendMessage("没有名为"+args[0]+"的菜单！");
 			return true;
@@ -43,19 +51,23 @@ public class Main extends PluginBase{
 			return true;
 		}
 		Map<String, Object> data = new LinkedHashMap<String, Object>(),
-				            buttons = new LinkedHashMap<String, Object>(),
-				            pair,button,image;
-		int i = 0;            
+		                    buttons = new LinkedHashMap<String, Object>(),
+		                    pair,button,image;
+		int i = 0;
+		String imagepath;
 		data.put("type", "form");
 		data.put("title", args[0]);
 		for(Map.Entry<String, Object> entry : ((LinkedHashMap<String,Object>)(conf.get(args[0]))).entrySet()){
 		    pair = (Map<String,Object>)entry.getValue();
 		    button = new LinkedHashMap<String, Object>();
-		       image = new LinkedHashMap<String, Object>();
-		    button.put("text",entry.getKey());
-		       image.put("type","path");
-		       image.put("data",pair.get("图标"));
-		    button.put("image",image);
+		    imagepath = (String)pair.get("图标");
+		    if(!(imagepath.equals("无"))){
+		        image = new LinkedHashMap<String, Object>();
+		        button.put("text",entry.getKey());
+		        image.put("type","path");
+		        image.put("data",imagepath);
+		        button.put("image",image);
+		    }
 		    buttons.put(String.valueOf(i),button);
 		    i++;
 		}
